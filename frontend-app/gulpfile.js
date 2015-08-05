539b01a2a439c4adc3f4aae22e2355ebbbf45584
jvee <juanvanni@fastmail.com>
2015-08-05 15:30:23 -0300

    gulpfile with reference tasks

---
+++ frontend-app/gulpfile.js
@@ -0,0 +1,60 @@
+/**
+ * Dependencies
+ */
+var gulp = require('gulp'),
+    $ = require('gulp-load-plugins')();
+
+
+// Compila los sass del proyecto y agrega vendor prefixes
+gulp.task('styles', function() {
+    return gulp.src(['styles/*.scss'])
+        .pipe($.sass({
+            outputStyle: 'expanded',
+            includePaths: ['.'],
+            onError: console.error.bind(console, 'Sass error:')
+        }))
+        .pipe($.postcss([
+            require('autoprefixer-core')({
+                browsers: ['last 5 versions', 'android >= 2.1', '> 1%']
+            })
+        ]))
+        .pipe($.size({
+            title: 'CSS size:'
+        }))
+        .pipe(gulp.dest('build'));
+});
+
+// Runs only in ie legacy stylesheets. Adds polyfill for opacity, filters, overflow ellipsis, clearfix
+gulp.task('stylesIE', function() {
+    return gulp.src('build/main.css')
+        .pipe($.postcss([
+            require('cssgrace')()
+        ]))
+        .pipe($.size({
+            title: 'IE styles size:'
+        }))
+        .pipe(gulp.dest('build/main-ie.css'));
+});
+
+// Minification Task for dist
+gulp.task('stylesDist', function() {
+    return gulp.src('build/*.css')
+        .pipe($.postcss([
+            require('cssnano')()
+        ]))
+        .pipe($.size({
+            title: 'CSS minified size:'
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+// Linting and Stats
+gulp.task('stylesInfo', ['styles'], function() {
+    return gulp.src('build/*.css')
+        .pipe($.postcss([
+            require('doiuse')({
+                browsers: ['last 5 versions', 'android >= 2.1', '> 1%']
+            }),
+            require('cssstats')()
+        ]))
+});
